2d9a47b764e8ca48792abe94244ed2dd51e167f1
Luciano Rossi <lukio@gcoop.coop>
2019-06-28 10:24:45 -0300

    Agrego tipo de comprobante que sera utilizado para crear facturas.

--- collect.py
+++ collect.py
@@ -17,6 +17,23 @@ class Collect(metaclass=PoolMeta):
             'readonly': Eval('state') != 'processing',
         },
         depends=['state'])
+    invoice_type = fields.Many2One('account.pos.sequence', 'Comprobante',
+        states={
+            'readonly': Eval('state') != 'processing',
+            'invisible': Eval('type') == 'send',
+        },
+        domain=[('pos', '=', Eval('pos'))], depends=['pos'])
+
+    @classmethod
+    def __setup__(cls):
+        super(Collect, cls).__setup__()
+        cls._buttons['create_invoices'].update({
+                'pre_validate':
+                ['AND',
+                    ('invoice_type', '!=', None),
+                    ('pos', '!=', None),
+                    ],
+                })
 
     @staticmethod
     def default_pos():
